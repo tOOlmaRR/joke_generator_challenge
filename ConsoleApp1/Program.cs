20059eb84bc53cd29c93eab1333a0f22790f6c85
tOOlmaRR <georgeumarr@hotmail.com>
2020-09-14 20:31:29 -0400

    add a try/catch wrapper to the main program
    include friendly error message, avoid potentially passing important information on to the end-user

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -17,64 +17,71 @@ namespace ConsoleApp1
 
         static void Main(string[] args)
         {
-            bool userWantsMoreJokes = true;
-            while (userWantsMoreJokes)
+            try
             {
-                printer.Value("Press c to get categories").ToString();
-                printer.Value("Press r to get random jokes").ToString();
-                GetEnteredKey(Console.ReadKey());
-                
-                // Load and display categories
-                if (key == 'c')
+                bool userWantsMoreJokes = true;
+                while (userWantsMoreJokes)
                 {
-                    getCategories();
-                    PrintResults();
-                }
-
-                // Let's tell some jokes
-                if (key == 'r')
-                {
-                    printer.Value("Want to use a random name? y/n").ToString();
+                    printer.Value("Press c to get categories").ToString();
+                    printer.Value("Press r to get random jokes").ToString();
                     GetEnteredKey(Console.ReadKey());
-                    if (key == 'y')
-                    { 
-                        GetNames();
-                    }
 
-                    printer.Value("How many jokes do you want? (1-9)").ToString();
-                    int n = 1;
-                    GetEnteredKey(Console.ReadKey());
-                    if (key > 1 && key <= 9)
+                    // Load and display categories
+                    if (key == 'c')
                     {
-                        n = key;
+                        getCategories();
+                        PrintResults();
                     }
 
-                    string? category = null;
-                    printer.Value("Want to specify a category? y/n").ToString();
-                    GetEnteredKey(Console.ReadKey());
-                    if (key == 'y')
+                    // Let's tell some jokes
+                    if (key == 'r')
                     {
-                        printer.Value("Enter a category (followed by the ENTER key): ").ToString();
-                        category = Console.ReadLine();
-                    }
-                    GetRandomJokes(category, n);
-                    PrintResults();
-                }
+                        printer.Value("Want to use a random name? y/n").ToString();
+                        GetEnteredKey(Console.ReadKey());
+                        if (key == 'y')
+                        {
+                            GetNames();
+                        }
 
-                printer.Value("Would you like to begin again? y/n").ToString();
-                GetEnteredKey(Console.ReadKey());
-                if (key == 'n')
-                {
-                    // user says exit, so let's make it so
-                    userWantsMoreJokes = false;
-                }
-                else
-                {
-                    // reinitialize things and allow the program to loop through again
-                    names = null;
+                        printer.Value("How many jokes do you want? (1-9)").ToString();
+                        int n = 1;
+                        GetEnteredKey(Console.ReadKey());
+                        if (key > 1 && key <= 9)
+                        {
+                            n = key;
+                        }
+
+                        string? category = null;
+                        printer.Value("Want to specify a category? y/n").ToString();
+                        GetEnteredKey(Console.ReadKey());
+                        if (key == 'y')
+                        {
+                            printer.Value("Enter a category (followed by the ENTER key): ").ToString();
+                            category = Console.ReadLine();
+                        }
+                        GetRandomJokes(category, n);
+                        PrintResults();
+                    }
+
+                    printer.Value("Would you like to begin again? y/n").ToString();
+                    GetEnteredKey(Console.ReadKey());
+                    if (key == 'n')
+                    {
+                        // user says exit, so let's make it so
+                        userWantsMoreJokes = false;
+                    }
+                    else
+                    {
+                        // reinitialize things and allow the program to loop through again
+                        names = null;
+                    }
                 }
             }
-
+            catch (Exception)
+            {
+                printer.Value("*** Something unexpected occurred. The appropriate joke administrators have been notified of the issue. Please try asking for more jokes again soon!").ToString();
+                // TODO: Log the actual exception somewhere so that we can troubleshoot what's going wrong; consider sending notifications if this a mission critical application!
+            }
         }
 
         private static void PrintResults()
